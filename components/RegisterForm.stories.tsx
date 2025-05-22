8fad6ee43f669c49daecace670c1a32d831e3046
akihito1015 <koutarou0827@icloud.com>
2025-05-22 18:40:20 +0900

    DAY1_1-2

--- components/RegisterForm.stories.tsx
+++ components/RegisterForm.stories.tsx
@@ -5,5 +5,25 @@ import type { Meta, StoryObj } from "@storybook/react";
 import RegisterForm from "./RegisterForm";
 
 // TODO: メタデータ
+const meta: Meta<typeof RegisterForm> = {
+  title: "Components/RegisterForm",
+  component: RegisterForm,
+};
+
+export default meta;
+
 // TODO: ストーリーの定義
+type Story = StoryObj<typeof RegisterForm>;
+
 // TODO: デフォルトストーリーの設定
+export const Default: Story = {
+  args: {
+    onSuccess: () => {
+      alert("登録に成功しました！");
+    },
+    onError: (error) => {
+      alert("エラーが発生しました: " + error?.message);
+    },
+    disabled: false,
+  },
+};
