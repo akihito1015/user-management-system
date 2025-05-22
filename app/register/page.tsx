5c67e570e2e3499a923bd3f293d0019194f9449e
akihito1015 <koutarou0827@icloud.com>
2025-05-22 16:48:36 +0900

    1-3完了しました。

---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React from 'react';
+import RegisterForm from '../../components/RegisterForm';
+
+const RegisterPage: React.FC = () => {
+  return <RegisterForm />;
+};
+
+export default RegisterPage;
