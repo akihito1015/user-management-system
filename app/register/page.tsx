6d9dfd19011944144fd3fc93bf4846ff7fc292ae
akihito1015 <koutarou0827@icloud.com>
2025-05-22 18:47:38 +0900

    DAY1_1-3修正途中

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+<<<<<<< Updated upstream
 import RegisterForm from '../../components/RegisterForm';
 
 const RegisterPage: React.FC = () => {
@@ -8,3 +9,20 @@ const RegisterPage: React.FC = () => {
 };
 
 export default RegisterPage;
+=======
+import { useRouter } from 'next/navigation';
+import RegisterForm from '../../components/RegisterForm';
+
+const RegisterPage: React.FC = () => {
+  const router = useRouter();
+
+  const handleSuccess = () => {
+    // 登録成功時にユーザー一覧ページへ遷移
+    router.push('/users');
+  };
+
+  return <RegisterForm onSuccess={handleSuccess} />;
+};
+
+export default RegisterPage;
+>>>>>>> Stashed changes
